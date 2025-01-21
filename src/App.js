f987e467b154b824bb50de3eba956870f1129df1
Lopes51789 <gablopesc10@gmail.com>
2025-01-21 17:26:24 -0300

    update App.css and App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <Navbar />
       <div className="hero">
         <video className="background-video" autoPlay loop muted>
-        <source src="/src/components/video.mp4" type="video/mp4" />
+        <source src="/src/components/video2.mp4" type="video/mp4" />
         Your browser does not support the video tag.
         </video>
         <div className="content">
